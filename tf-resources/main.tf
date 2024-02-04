40f75793ef7d04d055198336722bca3d9d675ec1
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2024-02-04 21:08:45 +0900

    add tf-resources.

---
+++ tf-resources/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      project = "multi-step-workflow-tf"
+    }
+  }
+}
